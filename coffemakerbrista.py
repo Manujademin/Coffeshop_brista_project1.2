7adaf4cc1842e5eaad4d8f245ceea045c756f74c
Manujademin <137299721+Manujademin@users.noreply.github.com>
2023-07-30 02:11:41 -0700

    Add files via upload

---
+++ coffemakerbrista.py
@@ -0,0 +1,46 @@
+#lets make a robot brista to work at a Resturent
+
+print("Heloo coustomer welcome to our coffe shop")
+ 
+name = input("what is you're name \n")
+
+print(name +",we'll bring you a menu soon as posible ")
+print("\n")
+#print("\n")
+
+print("--------menu-------------")
+print("Black Coffe")
+print("white coffe")
+print("cold coffe")
+print("Espresso")
+
+print("\n")
+#print("\n")
+
+print("what would you like to order? ")
+
+print("\n")
+
+
+order = input("May I take youre order? ")
+
+print("\n")
+print("Hey "+ name +" youre order will be ready in a moment...")
+
+#so i made a system that can get orders, Now let's make a system that can price items 
+#Im in a biginer stage so i have soo much to learn lets think everything on this menu is same price 
+#today is 07/22/2023 im Manuja Demin 
+
+#task 1 :giving price
+
+x = 5
+
+print("That is about :$",x,)
+#lets add Quantity
+
+Quantity = int(input("How much would you like to order: \n"))
+
+price = (Quantity * x)
+
+print("That will be :$"+str(price ))
+
